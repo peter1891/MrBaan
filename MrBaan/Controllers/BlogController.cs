ff3a2274f4fedac8e3af514e5edabd5fddb3c428
peter1891 <peter.baan@outlook.com>
2024-06-23 16:27:52 +0200

    Add project files.

---
+++ MrBaan/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MrBaan.Models;
+using System.Diagnostics;
+
+namespace MrBaan.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public BlogController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddBlog()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MrBaan/Controllers/ProjectsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MrBaan.Models;
+using System.Diagnostics;
+
+namespace MrBaan.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ProjectsController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddProject()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
